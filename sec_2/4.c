59994d88ac3e9151f5282fa91ab0777bd8dfdfd3
Alex Yin <yinchihkai0909@gmail.com>
2025-03-17 23:37:55 +0800

    feat: finish excercise

---
+++ sec_2/4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	float d;
+	float e;
+	float f;
+
+	printf("a: %d", a);
+	printf("b: %d", b);
+	printf("c: %d", c);
+	printf("d: %f", d);
+	printf("e: %f", e);
+	printf("f: %f", f);
+}
---
+++ sec_2/9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int answer;
+	int q = 6;
+	int p = 3;
+
+	answer = (3 * q - p * p) / 3;
+
+	return 0;
+}
